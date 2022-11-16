f50a107082859a44254a25fd752168a0437494c2
fatropolis <103893233+fatropolis@users.noreply.github.com>
2022-11-16 12:34:33 -0600

    Init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './app-home/home';
 
-const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HomeComponent},
+  { path: 'home', redirectTo: '' },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BgComponent } from './app-bg/bg';
+import { HomeComponent } from './app-home/home';
+import { MainComponent } from './app-main/main';
+import { NoteComponent } from './app-reusables/note/note';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { NotesService } from './app-services/notes.service';
+import { UIService } from './app-services/ui.service';
+import { UIComponent } from './app-ui/ui';
+import { MgComponent } from './mg-component/mg';
 
 @NgModule({
   declarations: [
-    AppComponent
+    MainComponent,
+    BgComponent,
+    MgComponent,
+    UIComponent,
+    HomeComponent,
+    NoteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [UIService,NotesService],
+  bootstrap: [MainComponent]
 })
 export class AppModule { }
